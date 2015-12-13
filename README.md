282dc13b03e07fb2a7e2859549391502f6a332d0
Matthieu Lux <matthieu.lux@gmail.com>
2015-12-13 16:04:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fountain Inject Generator
+
+Yeoman 'fountain' generator which handle the script injection.
+
+## Project under heavy development
---
+++ generators/app/index.js
@@ -0,0 +1,65 @@
+var fountain = require('fountain-generator');
+
+module.exports = fountain.Base.extend({
+  prompting: function () {
+    this.options.modules = 'inject';
+    this.fountainPrompting();
+  },
+
+  configuring: {
+    package: function () {
+      let dependencies;
+      let devDependencies = {};
+
+      this.updateJson('package.json', function (packageJson) {
+        dependencies = packageJson.dependencies;
+        delete packageJson.dependencies;
+        return packageJson;
+      });
+
+      this.mergeJson('package.json', {
+        devDependencies: {
+          'gulp-babel': '^6.1.0',
+          'gulp-inject': '^3.0.0',
+          'main-bower-files': '^2.9.0',
+          wiredep: '^2.2.2',
+          'gulp-angular-filesort': '^1.1.1'
+        }
+      });
+
+      if (this.props.framework === 'react') {
+        delete dependencies['react-dom'];
+      }
+
+      if (this.props.framework === 'angular1') {
+        devDependencies['angular-mocks'] = dependencies.angular;
+      }
+
+      this.mergeJson('bower.json', {
+        name: 'fountain-inject',
+        version: '0.0.1',
+        dependencies: dependencies,
+        devDependencies: devDependencies
+      });
+    }
+  },
+
+  writing: {
+    gulp: function () {
+      this.fs.copyTpl(
+        this.templatePath('gulp_tasks'),
+        this.destinationPath('gulp_tasks'),
+        { css: this.props.css }
+      );
+    },
+
+    indexHtml: function () {
+      this.replaceInFile('src/index.html', /<\/head>/, { head: true });
+      this.replaceInFile('src/index.html', /<\/html>/, { head: false });
+    }
+  },
+
+  installing: function () {
+    this.bowerInstall();
+  }
+});
