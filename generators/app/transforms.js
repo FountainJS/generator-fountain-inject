fdaaec07b996377c85cef05702ed05841b1e9e4f
Matthieu Lux <matthieu.lux@gmail.com>
2016-02-28 21:23:41 +0100

    add transformations for angular 1

--- generators/app/transforms.js
+++ generators/app/transforms.js
@@ -5,18 +5,23 @@ const path = require('path');
 module.exports = function transforms() {
   this.replaceInFiles('src/**/*.{js,ts,tsx}', (content, fileName) => {
     const baseName = path.basename(fileName, path.extname(fileName));
-    const componentName = baseName.substr(0, 1).toUpperCase() + baseName.substr(1);
+    const reactComponentName = baseName.substr(0, 1).toUpperCase() + baseName.substr(1);
+    const angular1ComponentName = baseName === 'main' ? 'app' : 'techs' + reactComponentName;
     // remove es2015 imports
     let result = content.replace(/import .*\n\n?/g, '');
     // remove commonjs requires
     result = result.replace(/.*require\(.*\);\n\n?/g, '');
     // add TS reference in files which doesn't have one
-    result = result.replace(
-      /^(?!\/\/\/ <reference path=)/g,
-      '/// <reference path="../../typings/tsd.d.ts" />\n\n'
-    );
+    if (this.props.js === 'typescript') {
+      const relativeFilePath = path.relative(this.destinationPath('src'), fileName);
+      const relativePath = relativeFilePath.split('/').map(() => '../').join('');
+      result = result.replace(
+        /^(?!\/\/\/ <reference path=)/g,
+        `/// <reference path="${relativePath}typings/tsd.d.ts" />\n\n`
+      );
+    }
     // remove exports of es2015 or typescript
-    result = result.replace(/(\n?\s*)export /g, '$1');
+    result = result.replace(/export /g, '');
     // remove exports of es2015 React components
     result = result.replace(
       /extends Component/g,
@@ -24,18 +29,28 @@ module.exports = function transforms() {
     );
     // remove exports of createClass React components
     result = result.replace(
-      /module\.exports = React\.createClass/,
-      `window.${componentName} = React.createClass`
+      /module\.exports = React\.createClass/g,
+      `window.${reactComponentName} = React.createClass`
     );
     // rename styles var for React inline style
     result = result.replace(
       /(var|const) styles =/g,
-      `$1 ${componentName}Styles =`
+      `$1 ${reactComponentName}Styles =`
     );
     result = result.replace(
       /style={styles\.(.*)}/g,
-      `style={${componentName}Styles.$1}`
+      `style={${reactComponentName}Styles.$1}`
     );
+    // remove centralized Angular 1 component declaration
+    if (baseName === 'index') {
+      result = result.replace(/\n\s+\.component[^;]*/g, '');
+      result = result.replace(/(\.module.*\[).*(\])/g, '$1$2');
+    } else {
+      result = result.replace(
+        /(const .*|module\.exports) = ([\s\S]*?(?=\n\})\n})/g, // ok, this one is ugly...
+        `angular.module('app').component('${angular1ComponentName}', $2)`
+      );
+    }
     return result;
   });
 };
